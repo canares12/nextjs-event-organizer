990262b21d953c621997ce4d81267320b13e0e79
canares12 <canarescr12@outlook.ph>
2024-03-18 22:43:11 +0800

    fourth commit

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -63,7 +63,6 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === "user.created") {
-    console.log(eventType);
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
