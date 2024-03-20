7c57b25f363b420766742a001926375a62025f7a
canares12 <canarescr12@outlook.ph>
2024-03-20 21:40:25 +0800

    update commit

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -1,123 +1,61 @@
-import { Webhook } from "svix";
+import prisma from "@/lib/db/prisma";
+import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
+import { IncomingHttpHeaders } from "http";
 import { headers } from "next/headers";
-import { WebhookEvent } from "@clerk/nextjs/server";
-import { CreateUserValues } from "@/lib/validation";
-import {
-  UpdateUserValues,
-  createUserFromClerk,
-  deleteUser,
-  updateUserFromClerk,
-} from "@/lib/db/user";
-import { handleError } from "@/lib/utils";
-import { clerkClient } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import { Webhook, WebhookRequiredHeaders } from "svix";
 
-export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+const webhookSecret = process.env.WEBHOOK_SECRET || "";
 
-  if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
-    );
-  }
-
-  // Get the headers
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
-
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
-  }
-
-  // Get the body
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
-
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
-
+async function handler(request: Request) {
+  const payload = await request.json();
+  const headersList = headers();
+  const heads = {
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+  };
+  const wh = new Webhook(webhookSecret);
   let evt: WebhookEvent;
 
   // Verify the payload with the headers
   try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) as WebhookEvent;
+    evt = wh.verify(
+      JSON.stringify(payload),
+      heads as IncomingHttpHeaders & WebhookRequiredHeaders,
+    ) as WebhookEvent;
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
+    console.error((err as Error).message);
+    return NextResponse.json({}, { status: 400 });
   }
 
-  // Get the ID and type
-  const { id } = evt.data;
   const eventType = evt.type;
-
-  if (eventType === "user.created") {
+  if (eventType === "user.created" || eventType === "user.updated") {
+    // const { id, ...attributes } = evt.data;
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
-    const user: CreateUserValues = {
-      clerkId: id,
-      username: username!,
-      email: email_addresses[0].email_address,
-      firstName: first_name ? first_name : null,
-      lastName: last_name ? last_name : null,
-      photo: image_url,
-    };
-
-    try {
-      const newUser = await createUserFromClerk(user);
-      if (newUser) {
-        await clerkClient.users.updateUserMetadata(id, {
-          publicMetadata: {
-            userId: newUser.id,
-          },
-        });
-      }
-      return NextResponse.json({ message: "OK", user: newUser });
-    } catch (error) {
-      handleError(error);
-    }
+    await prisma.user.upsert({
+      where: { clerkId: id },
+      create: {
+        clerkId: id,
+        username: username!,
+        email: email_addresses[0].email_address,
+        firstName: first_name,
+        lastName: last_name,
+        photo: image_url,
+      },
+      update: { 
+        username: username!,
+        firstName: first_name,
+        lastName: last_name,
+        photo: image_url,
+       },
+    });
   }
-
-  if (eventType === "user.updated") {
-    const { id, username, first_name, last_name, image_url } = evt.data;
-
-    const user: UpdateUserValues = {
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    };
-
-    try {
-      const updatedUser = await updateUserFromClerk(id, user);
-
-      return NextResponse.json({ message: "OK", user: updatedUser });
-    } catch (error) {
-      handleError(error);
-    }
-  }
-
-  if (eventType === "user.deleted") {
-    try {
-      const deletedUser = await deleteUser(id!);
-
-      return NextResponse.json({ message: "OK", user: deletedUser });
-    } catch (error) {
-      handleError(error);
-    }
-  }
-
   return new Response("", { status: 200 });
 }
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
